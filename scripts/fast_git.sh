319c4d14dd03e649bfde883cec47eed05811b1fb
Ammon Taylor <ammonx9@gmail.com>
2024-09-15 13:21:09 -0600

    fast pr

--- scripts/fast_git.sh
+++ scripts/fast_git.sh
@@ -1,15 +1,14 @@
 # fast git add, commit, and push
 function fgit() {
-    git add -A 
+    git add -A
     git commit -m "$1"
     git push
 }
 
 # fast git merge
 function fmerge() {
-    local cbranch=`git branch |grep \* | cut -d ' ' -f2`
-    if [ $# -eq 0 ]
-    then
+    local cbranch=$(git branch | grep \* | cut -d ' ' -f2)
+    if [ $# -eq 0 ]; then
         git checkout main
         git pull
         git checkout $cbranch
@@ -20,4 +19,14 @@ function fmerge() {
         git checkout $cbranch
         git merge $1
     fi
+}
+
+# create a draft PR for the current branch
+function fpr() {
+    local cbranch=$(git branch --show-current)
+    if [ -z "$1" ]; then
+        echo "Usage: fdraftpr <PR name>"
+        return 1
+    fi
+    gh pr create --title "$1" --draft --base main --head "$cbranch"
 }
